ccb501fbce7b18e2c7dcd78639fb91e34ff7507c
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-31 09:34:05 +0100

    ADD ex-03 Array Facile | for-of cycle solution

--- ex-03/array_facile.js
+++ ex-03/array_facile.js
@@ -1,4 +1,11 @@
 const array = [1, 3, 5, 6, 87, 7, 9, 64, 22, 1, 12];
-const evenArray = array.filter((num) => num % 2 === 0);
+//const evenArray = array.filter((num) => num % 2 === 0);
+let evenArray = [];
+
+for (num of array) {
+    if (num % 2 === 0) {
+        evenArray.push(num);
+    }
+}
 
 console.log(evenArray);
